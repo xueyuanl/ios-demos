aa0a5e916913735875a343e528e875c330d1a38c
hsuehyuanli <15186846+xueyuanl@users.noreply.github.com>
2020-09-26 23:28:05 +0800

    add landmarks demo

---
+++ landmarks/landmarks/landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by hsuehyuan li on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
